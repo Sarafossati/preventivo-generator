891e0b459a4252f302a088d614f1ee32a36dcb84
Sara Fossati <sarafossati6@gmail.com>
2022-02-25 17:11:41 +0100

    first-update

---
+++ app.js
@@ -0,0 +1,46 @@
+const getNomeCognome = () => {
+
+    let nome = itemUpperCase(document.getElementById("nome").value);
+    let cognome = itemUpperCase(document.getElementById("cognome").value);
+    let nomeCognome = nome + " " + cognome;
+
+    addInHtml("valueCliente", nomeCognome)
+}
+
+const getFatturazione = () => {
+
+    let indirizzo = itemUpperCase(document.getElementById("indirizzo").value);
+    let civico = document.getElementById("civico").value;
+    let citta = itemUpperCase(document.getElementById("citta").value);
+    let cap = document.getElementById("cap").value;
+    let provincia = (document.getElementById("provincia").value).toUpperCase();
+    let fatturazione = indirizzo + ", " + civico + ". " + citta + " - " + cap + " - (" + provincia + ").";
+
+    addInHtml("valueFatturazione", fatturazione)
+}
+
+const getIva = () => {
+
+    let iva = "P.Iva: " + (document.getElementById("iva").value);
+
+    addInHtml("valueIva", iva)
+}
+
+const getCf = () => {
+
+    let cf = "CF:" + (document.getElementById("cf").value).toUpperCase();
+
+    addInHtml("valueCf", cf)
+}
+
+// FIRT LETTER UPPERCASE
+const itemUpperCase = (item) => {
+    return item.charAt(0).toUpperCase() + item.slice(1);
+}
+
+// ADD ELEMENT IN HTML
+const addInHtml = (element, elementHtml) => {
+    return document.getElementById(element).innerHTML = (elementHtml)
+}
+
+
